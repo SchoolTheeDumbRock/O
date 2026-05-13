8ed946beeeb3c9d8a34616fa9622b9c3059f9430
Dom <8043168@student.ssttx.org>
2026-05-13 18:44:18 +0000

    moop

--- O.py
+++ O.py
@@ -4,7 +4,15 @@ MUL = '*'
 DIVIDE = '/'
 NUM = int
 OP = [ADD, MINUS, MUL, DIVIDE]
+
 # Look up how to use the .split to make tokens
-o = input("O:")
-tokens = o.split()
+
+o = input("O:") #input
+
+tokens = o.split(" ") #tokenize process and process info to determine stuff like operation
+
+ostack = []                   #the stack
+stack = ostack.append(tokens) #the stack
+
 print(tokens)
+print(ostack)
